6abcea7ee06cd050ed402fb61950b28a2cd00f48
Jack <jmcgregor@spartaglobal.com>
2018-09-26 20:13:27 +0100

    restructured for SASS

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,8 +7,6 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm">
         <div className="container">
           <Link className="navbar-brand" to="/">IMC</Link>
-          <Link to="/courses" className="nav-link px-2">Courses</Link>
-          <Link to="/cohorts" className="nav-link px-2">Cohorts</Link>
           <ul className="navbar-nav ml-auto d-flex flex-row">
             <li className="nav-item active">
               <Link to="/login" className="nav-link px-2">Login</Link>
